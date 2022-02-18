e5057b1d0726ed1ab2707bdd5a2567545286d9f7
Ali Spittel <ali07cat07@gmail.com>
2022-02-17 19:05:03 -0700

    add dark mode challenge

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,22 @@
+import { useState } from 'react'
+
 export default function DarkMode () {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${darkMode && 'dark-mode'}`}>
+      <button
+        className='dark-mode-button'
+        onClick={() => setDarkMode(true)}
+      >
+        Dark Mode
+      </button>
+      <button
+        className='light-mode-button'
+        onClick={() => setDarkMode(false)}
+      >
+        Light Mode
+      </button>
     </div>
   )
 }
