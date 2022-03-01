dddf5b7be1351bc97beb07a7e8e04f857d95c1ae
Ali Spittel <ali07cat07@gmail.com>
2022-02-28 18:34:53 -0700

    completed code

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
+
+const getDogPic = async () => {
+  const response = await fetch('https://dog.ceo/api/breeds/image/random')
+  const dog = await response.json()
+  return dog.message
+}
+
 export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
+  const [dogPic, setDogPic] = useState('')
+
+  useEffect(() => {
+    const getDog = async () => setDogPic(await getDogPic())
+    getDog()
+  }, [])
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogPic} />
+      <button onClick={async e => setDogPic(await getDogPic())}>🐶</button>
     </div>
   )
 }
