ddfceaacc595fd35af4c7dfad288e1c02f396bd7
Ali Spittel <ali07cat07@gmail.com>
2022-02-28 18:13:57 -0700

    add solution for challenge 3

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -5,8 +5,29 @@ export default function FormValidator () {
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
 
+  const [message, setMessage] = useState('')
+
+  const findErrors = () => {
+    const errors = []
+    if (!email || !password || !passwordConfirm) errors.push('All fields must be filled in')
+    if ([...email].filter(i => i === '@').length !== 1) {
+      errors.push('An email must have exactly one @ sign')
+    }
+    if (password.length <= 8) errors.push('Passwords must be 8 characters or longer')
+    if (password !== passwordConfirm) errors.push('Passwords must match')
+
+    return errors
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const errors = findErrors()
+    setMessage(errors.length ? errors.join(', ') : 'User created!')
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,6 +44,7 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
+      {message}
       <input type='submit' value='Submit' />
     </form>
   )
