51a16ad07feacb9932340e5dccb8c7b6de87d942
Ali Spittel <ali07cat07@gmail.com>
2022-03-31 22:12:04 -0600

    finished code

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -12,7 +12,36 @@ const items = [{
 }]
 
 function App () {
-  const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
+  const [cart, setCart] = useState([])
+
+  const addToCart = (item) => {
+    const cartCopy = [...cart]
+    const itemInCart = cartCopy.find(i => item.name === i.name)
+    if (itemInCart) {
+      itemInCart.quantity += 1
+      setCart(cartCopy)
+    } else {
+      setCart(prevCart => [...prevCart, { ...item, quantity: 1 }])
+    }
+  }
+
+  const increase = (name) => {
+    const cartCopy = [...cart]
+    const item = cartCopy.find(i => i.name === name)
+    item.quantity += 1
+    setCart(cartCopy)
+  }
+
+  const decrease = (name) => {
+    let cartCopy = [...cart]
+    const item = cartCopy.find(i => i.name === name)
+    if (item.quantity > 1) {
+      item.quantity -= 1
+    } else {
+      cartCopy = cartCopy.filter(i => i.name !== name)
+    }
+    setCart(cartCopy)
+  }
 
   return (
     <div>
@@ -24,27 +53,29 @@ function App () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={() => addToCart(item)}>Add to Cart</button>
             </div>)
           )}
         </div>
         <div>
           <h2>Cart</h2>
-          {cart.map(item => (
-            <div key={item.name}>
-              <h3>{item.name}</h3>
-              <p>
-                <button>-</button>
-                {item.quantity}
-                <button>+</button>
-              </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
-            </div>
-          ))}
+          {cart.length > 0
+            ? cart.map(item => (
+              <div key={item.name}>
+                <h3>{item.name}</h3>
+                <p>
+                  <button onClick={() => decrease(item.name)}>-</button>
+                  {item.quantity}
+                  <button onClick={() => increase(item.name)}>+</button>
+                </p>
+                <p>Subtotal: ${Math.round(item.quantity * item.price * 100) / 100}</p>
+              </div>
+              ))
+            : <p>Add an item to your cart</p>}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: ${cart.reduce((acc, i) => acc + (i.quantity * i.price), 0)}</h2>
       </div>
     </div>
   )
