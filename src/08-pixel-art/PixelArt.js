552029b3e725d0085a34500ff46d89dcd57ffa56
Ali Spittel <ali07cat07@gmail.com>
2022-04-03 13:29:33 -0600

    Update PixelArt.js

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -16,7 +16,7 @@ function Pixel () {
 
 function Pixels () {
   const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel />)
+  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
       {pixels}
